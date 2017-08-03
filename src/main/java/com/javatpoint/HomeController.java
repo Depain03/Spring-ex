ff00f8235024e69713412b3f88b076addb3e3463
Depain03 <bdepain@yahoo.com>
2017-08-02 22:18:23 -0700

    1st commit

---
+++ src/main/java/com/javatpoint/HomeController.java
@@ -0,0 +1,16 @@
+package com.javatpoint;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+@RestController  
+public class HomeController {  
+    @RequestMapping(value="/hello", method=RequestMethod.GET)  
+    public String hello() { 
+    	String json = "http://headers.jsontest.com/";
+    	RestTemplate restTemplate = new RestTemplate();
+    	String result=restTemplate.getForObject(json, String.class );
+    	System.out.println(result);
+				return result;  
+}  
+}
